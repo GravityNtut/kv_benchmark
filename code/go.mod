6148385445113452847087bf0a8b16ffdb2a3bde
taroBill <bill890914@gmail.com>
2024-06-24 12:53:38 +0800

    init

---
+++ code/go.mod
@@ -0,0 +1,23 @@
+module nats-bench
+
+go 1.21.3
+
+require (
+	github.com/cucumber/godog v0.14.1
+	github.com/nats-io/nats.go v1.36.0
+)
+
+require (
+	github.com/cucumber/gherkin/go/v26 v26.2.0 // indirect
+	github.com/cucumber/messages/go/v21 v21.0.1 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-memdb v1.3.4 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
---
+++ code/main_test.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/cucumber/godog"
+	"github.com/nats-io/nats.go"
+	"go.k6.io/k6/cmd"
+)
+
+var natsUrl string
+var natsMonitoringUrl string
+var natsMonitoringPort string
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:        "pretty",
+			Paths:         []string{"./"},
+			StopOnFailure: true,
+			TestingT:      t,
+		},
+	}
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func ConnectToJS(natsUrl string) (*nats.Conn, nats.JetStreamContext, error) {
+	n, err := nats.Connect(natsUrl)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to connect to nats: %w", err)
+	}
+
+	js, err := n.JetStream()
+	if err != nil {
+		n.Close()
+		return nil, nil, fmt.Errorf("failed to connect to jetstream: %w", err)
+	}
+	return n, js, nil
+}
+
+func InitNatsUrl(url string) error {
+	natsUrl = url
+	return nil
+}
+
+func InitNatsMonitoringUrl(url, port string) error {
+	natsMonitoringUrl = url
+	natsMonitoringPort = port
+	return nil
+}
+
+func CreateBucket(bucketName string) error {
+	n, js, err := ConnectToJS(natsUrl)
+	if err != nil {
+		return err
+	}
+	defer n.Close()
+
+	js.CreateKeyValue(&nats.KeyValueConfig{Bucket: bucketName})
+	return nil
+}
+
+func InitBucket(keyCount int, bucketName string, keySize int) error {
+	n, js, err := ConnectToJS(natsUrl)
+	if err != nil {
+		return err
+	}
+	defer n.Close()
+
+	kv, err := js.KeyValue(bucketName)
+	if err != nil {
+		return fmt.Errorf("failed to get bucket: %w", err)
+	}
+
+	for i := 0; i < keyCount; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		value := make([]byte, keySize)
+		if _, err := kv.Put(key, value); err != nil {
+			return fmt.Errorf("failed to put key: %w", err)
+		}
+	}
+	return nil
+}
+
+func RunPerformanceTest(bucketName string, keyCount int, keySize int, concurrentUser int) error {
+	cmd.Execute()
+	return nil
+}
+
+func DeleteBucket(bucketName string) error {
+	n, js, err := ConnectToJS(natsUrl)
+	if err != nil {
+		return err
+	}
+	defer n.Close()
+
+	js.DeleteKeyValue(bucketName)
+	return nil
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Before(func(ctx context.Context, _ *godog.Scenario) (context.Context, error) {
+		return ctx, nil
+	})
+
+	ctx.Given(`^I have a nats with url "([^"]*)"$`, InitNatsUrl)
+	ctx.Given(`^The nats server monitoring url is "([^"]*)" and port is "([^"]*)"$`, InitNatsMonitoringUrl)
+	ctx.Given(`^I have a jetstream bucket named "([^"]*)"$`, CreateBucket)
+	ctx.Given(`^Create "([^"]*)" keys in "([^"]*)" bucket, per key size "([^"]*)" bytes$`, InitBucket)
+
+	ctx.Then(`^Delete "([^"]*)" bucket$`, DeleteBucket)
+}
