1d93b1f006291d032caad243e3fb9f4e990c8e5b
bill <bill89091@gmail.com>
2024-09-03 17:06:11 +0800

    feature: add analysis program

--- script/performance_test.sh
+++ script/performance_test.sh
@@ -5,12 +5,12 @@ k8s_setup_data_dir=/home/selab/Desktop/kv_benchmark/app
 nats_pv_dir=/home/selab/hdd
 nats_url=http://127.0.0.1:30000
 bucket_name=bucket
-times_test_run=5
+times_test_run=100
 
 # concurrent_user_array=(1 2 4 8 16 32 64 128 256 512)
-concurrent_user_array=(16 256 512)
+concurrent_user_array=(1024)
 # payload_array=("8" "16" "32" "64" "128" "256" "512" "1k" "2k" "4k" "8k" "16k" "32k" "64k" "128k" "256k" "512k" "1M")
-payload_array=("8" "16" "32")
+payload_array=("8k")
 
 # $1=$test_name
 init_nats_kv() {
@@ -36,7 +36,7 @@ clean_environment() {
 put_test(){
 	test_time=$(date -d "now" +%Y%m%d-%H%M)
 	nats -s $nats_url bench put_test --kv --multisubject --bucket $bucket_name --storage file --msgs $1 --size $2 --pub $3 --dedup --request --no-progress 2>/dev/null \
-	| mawk -v test_time="$test_time" '/Pub stats: /{printf "p@ %s@ %s@ %s@ %s@ %s@ %s\n", '"$2"', '"$3"', $3, $6, $7, test_time}' \
+	| mawk -v test_time="$test_time" -v payload_size="$2" -v concurrent_putter_amount="$3" '/Pub stats: /{printf "p@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_putter_amount, $3, $6, $7, test_time}' \
 	| sed -e 's/,//g' -e 's/@/,/g' \
 	>> $4
 }
@@ -45,7 +45,7 @@ put_test(){
 get_test(){
 	test_time=$(date -d "now" +%Y%m%d-%H%M)
 	nats -s $nats_url bench get_test --kv --multisubject --bucket $bucket_name --storage file --msgs $1 --size $2 --sub $3 --dedup --request --no-progress 2>/dev/null \
-	| mawk -v test_time="$test_time" '/Sub stats: /{printf "s@ %s@ %s@ %s@ %s@ %s@ %s\n", '"$2"', '"$3"', $3, $6, $7, test_time}' \
+	| mawk -v test_time="$test_time" -v payload_size="$2" -v concurrent_putter_amount="$3" '/Sub stats: /{printf "s@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_putter_amount, $3, $6, $7, test_time}' \
 	| sed -e 's/,//g' -e 's/@/,/g' \
 	>> $4
 }
@@ -54,7 +54,7 @@ get_test(){
 put_get_test(){
 	test_time=$(date -d "now" +%Y%m%d-%H%M)
 	nats -s $nats_url bench put_get_test --kv --multisubject --bucket $bucket_name --storage file --msgs $1 --size $2 --pub $3 --sub $4 --dedup --request --no-progress 2>/dev/null \
-	| mawk -v test_time="$test_time" '/ Pub stats: /{printf "p@ %s@ %s@ %s@ %s@ %s@ %s\n", '"$2"', '"$3"', $3, $6, $7, test_time} / Sub stats: /{printf "s@ %s@ %s@ %s@ %s@ %s@ %s\n", '"$2"', '"$4"', $3, $6, $7, test_time}' \
+	| mawk -v test_time="$test_time" -v payload_size="$2" -v concurrent_putter_amount="$3" -v concurrent_getter_amount="$4" '/ Pub stats: /{printf "p@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_putter_amount, $3, $6, $7, test_time} / Sub stats: /{printf "s@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_getter_amount, $3, $6, $7, test_time}' \
 	| sed -e 's/,//g' -e 's/@/,/g' \
 	>> $5
 }
@@ -97,14 +97,19 @@ run_concurrent_user_test(){
 	done
 }
 
+
+# $1=test_name $2=append_mode 
 set -e
 
-mkdir -p $csv_path/$1/$2
-echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_test.csv
-echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/get_test.csv
-echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_get_test.csv
+if [ $2 != true ]
+then
+	mkdir -p $csv_path/$1
+	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_test.csv
+	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/get_test.csv
+	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_get_test.csv
+fi
 
 for payload in "${payload_array[@]}"
 do
-	run_concurrent_user_test $1 10000 $payload
+	run_concurrent_user_test $1 1000000 $payload
 done
