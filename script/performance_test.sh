6f118892169129a6dfb10c7d21aaec4fcf62c987
bill <bill89091@gmail.com>
2024-10-01 16:16:58 +0800

    fix: make script check total msg size and fix script not handle nats comment fail problem

--- script/performance_test.sh
+++ script/performance_test.sh
@@ -5,12 +5,16 @@ k8s_setup_data_dir=/home/selab/Desktop/kv_benchmark/app
 nats_pv_dir=/home/selab/hdd
 nats_url=http://127.0.0.1:30000
 bucket_name=bucket
-times_test_run=10
+# times_test_run=10
+times_test_run=3
 
 # concurrent_user_array=(1 2 4 8 16 32 64 128 256 512 1024)
-concurrent_user_array=(32 128)
+concurrent_user_array=(32)
 # payload_array=("8" "16" "32" "64" "128" "256" "512" "1k" "2k" "4k" "8k" "16k" "32k" "64k" "128k" "256k" "512k" "1M")
-payload_array=("256" "1k" "4k" "8k")
+payload_array=("256")
+
+# payload_size * msg_amount(default:1000000) = 10 GB = 10737418240 B / 8G:8589934592B
+max_total_size=10737418240
 
 # $1=$test_name
 init_nats_kv() {
@@ -32,38 +36,73 @@ clean_environment() {
 	# && rm -r $nats_pv_dir
 }
 
-# $1=msg_amount  $2=payload_size  $3=concurrent_amount  $4=file_path  $5=concurrent_proportion (percentage for put, the rest for get e.g., 50=50%)
+# $1=msg_amount  $2=payload_size  $3=concurrent_amount  $4=file_path $5=concurrent_proportion (percentage for put, the rest for get e.g., 50=50%)
 put_get_test(){
-	test_time=$(date -d "now" +%Y%m%d-%H%M%S)
-
-	if [ "$#" -eq 5 ]
+	local _csv_path=$4
+	if [ $5 -eq 100 ]
+	then 
+		_csv_path+="put_test.csv"
+	elif [ $5 -eq 0 ]
 	then
-		concurrent_putter_amount=$(($3*$5/100))
-		concurrent_getter_amount=$(($3-$concurrent_putter_amount))
+		_csv_path+="get_test.csv"
 	else
-		echo "Invalid parameters"
-		exit 1
+		_csv_path+="put_get_test.csv"
 	fi
 
-	nats -s $nats_url bench put_get_test --kv --multisubject --bucket $bucket_name --storage file --msgs $1 --size $2 --pub $concurrent_putter_amount --sub $concurrent_getter_amount --dedup --request --no-progress 2>/dev/null \
-	| mawk -v test_time="$test_time" -v payload_size="$2" -v concurrent_putter="$concurrent_putter_amount" -v concurrent_getter="$concurrent_getter_amount" '$1=="Pub" {printf "p@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_putter, $3, $6, $7, test_time} $1=="Sub" {printf "s@ %s@ %s@ %s@ %s@ %s@ %s\n", payload_size, concurrent_getter, $3, $6, $7, test_time}' \
-	| sed -e 's/,//g' -e 's/@/,/g' \
-	>> $4
-}
+	local temp_out_path=$4"temp.txt"
 
+	test_time=$(date -d "now" +%Y%m%d-%H%M%S)
+
+	concurrent_putter_amount=$(($3*$5/100))
+	concurrent_getter_amount=$(($3-$concurrent_putter_amount))
+
+	nats -s $nats_url bench put_get_test --kv --multisubject --bucket $bucket_name --storage file --msgs $1 --size $2 --pub $concurrent_putter_amount --sub $concurrent_getter_amount --dedup --request --no-progress > $temp_out_path 2>&1 
+	nats_exit_stat=$?
+	if [ $nats_exit_stat -eq 0 ]
+	then
+		echo "nats success!"	
+		mawk -v test_time="$test_time" -v msg_amount="$1" -v payload_size="$2" -v concurrent_putter="$concurrent_putter_amount" -v concurrent_getter="$concurrent_getter_amount" '$1=="Pub" {printf "p@ %s@ %s@ %s@ %s@ %s@ %s@ %s\n", msg_amount, payload_size, concurrent_putter, $3, $6, $7, test_time} $1=="Sub" {printf "s@ %s@ %s@ %s@ %s@ %s@ %s@ %s\n", msg_amount, payload_size, concurrent_getter, $3, $6, $7, test_time}' $temp_out_path \
+			| sed -e 's/,//g' -e 's/@/,/g' \
+			>> $_csv_path
+	else
+		echo "nats failed!"
+		echo "msg:" $(tail -n 1 $temp_out_path)
+	fi
+}
 
 # $1=test_name $2=msg_amount $3=payload_size
 run_concurrent_user_test(){
 	for user_amount in "${concurrent_user_array[@]}"
 	do
 		init_nats_kv $1 
+
+		if grep -q "k" <<< $3
+		then
+			payload_size_translated=$((${3%k}*1024))
+		elif grep -q "M" <<< $3
+		then
+			payload_size_translated=$((${3%M}*1024*1024))
+		else # Byte
+			payload_size_translated=$(($3))
+		fi
+
+		msg_amount=$2
+		total=$(($msg_amount*$payload_size_translated))
+		while [ $total -gt $max_total_size ]
+		do
+			echo "Total size exceeds the limit"
+			msg_amount=$(($msg_amount/10))
+			total=$(($msg_amount*$payload_size_translated))
+		done
+
 		for ((i=1; i<=$times_test_run; i++))
 		do
 			echo "======= Starting put test $i ======="
 			echo "| Concurrent users: $user_amount"
+			echo "| msg amount: $msg_amount"
 			echo "| Payload size: $3"
 			echo "===================================="
-			put_get_test $2 $3 $user_amount $csv_path/$1/put_test.csv 100
+			put_get_test $msg_amount $3 $user_amount $csv_path/$1/ 100
 			sleep 10
 		done
 		
@@ -71,9 +110,10 @@ run_concurrent_user_test(){
 		do
 			echo "======= Starting get test $i ======="
 			echo "| Concurrent users: $user_amount"
+			echo "| msg amount: $msg_amount"
 			echo "| Payload size: $3"
 			echo "===================================="
-			put_get_test $2 $3 $user_amount $csv_path/$1/get_test.csv 0
+			put_get_test $msg_amount $3 $user_amount $csv_path/$1/ 0
 			sleep 10
 		done
 
@@ -81,9 +121,10 @@ run_concurrent_user_test(){
 		do
 			echo "===== Starting put get test $i ====="
 			echo "| Concurrent users: $user_amount"
+			echo "| msg amount: $msg_amount"
 			echo "| Payload size: $3"
 			echo "===================================="
-			put_get_test $2 $3 $user_amount $csv_path/$1/put_get_test.csv 50
+			put_get_test $msg_amount $3 $user_amount $csv_path/$1/ 50
 			sleep 10
 		done
 		clean_environment
@@ -92,17 +133,19 @@ run_concurrent_user_test(){
 
 
 # $1=test_name $2=append_mode 
-set -e
+# set -e
 
 if [ $2 != true ]
 then
 	mkdir -p $csv_path/$1
-	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_test.csv
-	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/get_test.csv
-	echo "type,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_get_test.csv
+	echo "type,msg_amount,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_test.csv
+	echo "type,msg_amount,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/get_test.csv
+	echo "type,msg_amount,payload_size,concurrent_user,total_msgs,throughput,throughput_unit,time" > $csv_path/$1/put_get_test.csv
 fi
 
 for payload in "${payload_array[@]}"
 do
 	run_concurrent_user_test $1 1000000 $payload
 done
+
+echo "Test finished!"
